607533e58a3e761b827648fd61d1f345996a6135
Ekaterina <katharina00000@gmail.com>
2024-05-28 06:57:00 +0300

    1st task 1-module

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+  let x = n - 1;
+  
+  for(let i = x; i > 0; i--) {
+    n *= i;
+  }
+
+  if(n === 0) {
+    return 1;
+  } else {
+    return n;
+  }
 }
+
+console.log( factorial() );
