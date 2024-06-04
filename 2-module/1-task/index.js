390e6f89d5a261f374ece791cb1652fef20e8be1
Ekaterina <katharina00000@gmail.com>
2024-06-04 06:29:58 +0300

    1st task 2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary (salaries) {
+  let sum = 0;
+  
+  for (let sal in salaries) {
+      if(typeof salaries[sal] === 'number' && Number.isFinite(salaries[sal]))   {
+          sum += salaries[sal];
+      }
+  }
+  return sum;
 }
+
+console.log( sumSalary(salaries) );
